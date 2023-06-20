85ec75d368560d3340a7a3f1c8373d90039d744c
elvisonob <elvisonob@yahoo.com>
2023-06-20 07:40:22 +0100

    homepage set

---
+++ components/home-page/featured-posts.js
@@ -0,0 +1,9 @@
+function FeaturedPost() {
+  return (
+    <div>
+      <h1>Oluwa is great</h1>
+    </div>
+  );
+}
+
+export default FeaturedPost;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import Hero from '../components/home-page/hero';
+//import FeaturedPost from '../components/home-page/featured-posts';
+
+function HomePage() {
+  return (
+    <Fragment>
+      <Hero />
+    </Fragment>
+  );
+}
+
+export default HomePage;
