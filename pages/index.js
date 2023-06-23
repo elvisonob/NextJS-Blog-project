ed8eb134bbb02ace928efbd00d5dc75c70663e92
elvisonob <elvisonob@yahoo.com>
2023-06-23 18:20:15 +0100

    featured-post set

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,43 @@
 import { Fragment } from 'react';
 import Hero from '../components/home-page/hero';
-//import FeaturedPost from '../components/home-page/featured-posts';
+import FeaturedPosts from '../components/home-page/featured-posts';
+
+const DUMMY_POSTS = [
+  {
+    slug: 'getting-started-with-nextjs',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is a React framework for production',
+    date: '2023-03-10',
+  },
+  {
+    slug: 'getting-started-with-nextjs2',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is a React framework for production',
+    date: '2023-03-10',
+  },
+  {
+    slug: 'getting-started-with-nextjs3',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is a React framework for production',
+    date: '2023-03-10',
+  },
+  {
+    slug: 'getting-started-with-nextjs4',
+    title: 'Getting started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is a React framework for production',
+    date: '2023-03-10',
+  },
+];
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
